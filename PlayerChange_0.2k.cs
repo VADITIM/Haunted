ad8b3ff2b03069811c5d7ef50daea4c004904804
CKJQLS3\Teilnehmer <KazimToker@live.de>
2024-08-22 09:34:05 +0200

    switch charakter mit X, erstellt ein create empty und nennt es beispiel player und dann noch ein empty das bennent ihr switsch player oder so und die zwei player ( also dann ersten einfach kopieren und die größe und kamera position ändern ) als child charakter unter dem switch player rein tun, dann sollte es funktionieren

---
+++ PlayerChange_0.2k.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChange : MonoBehaviour
+{
+    public int selectedPlayer = 0;
+
+    void Start()
+    {
+        SelectPlayer(); // Initialize the selected player at the start
+    }
+
+    void Update()
+    {
+        int previousSelectedPlayer = selectedPlayer;
+
+        // Handle player selection input
+        HandleInput();
+
+        // If the selected player has changed, update the active player
+        if (previousSelectedPlayer != selectedPlayer)
+        {
+            SelectPlayer();
+        }
+    }
+
+    void HandleInput()
+    {
+        // Switch to the next player
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            selectedPlayer = (selectedPlayer >= transform.childCount - 1) ? 0 : selectedPlayer + 1;
+        }
+        // Switch to the previous player
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            selectedPlayer = (selectedPlayer <= 0) ? transform.childCount - 1 : selectedPlayer - 1;
+        }
+    }
+
+    void SelectPlayer()
+    {
+        int i = 0;
+        foreach (Transform player in transform)
+        {
+            player.gameObject.SetActive(i == selectedPlayer);
+            i++;
+        }
+    }
+}
