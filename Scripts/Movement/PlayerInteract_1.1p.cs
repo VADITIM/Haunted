3f93d551523d1baf9a7857f199dcc90c690d59b8
Vadim Niedental <vadim.niedental@gmail.com>
2024-09-17 19:36:30 +0200

    Fixed Bug with Interaction

---
+++ Scripts/Movement/PlayerInteract_1.1p.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField]
+    private float distance = 3f;
+    [SerializeField]
+    private LayerMask mask;
+    private PlayerUI playerUI;
+    private InputManager inputManager; 
+    
+    void Start()
+    {
+        cam = GetComponent<PlayerLook>().cam;
+        playerUI = GetComponent<PlayerUI>();
+        inputManager = GetComponent<InputManager>();
+    }
+
+    void Update()
+    {
+        playerUI.UpdateText(string.Empty);
+        
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * distance, Color.red);
+        RaycastHit hitInfo; // variable to store our collision information
+        if (Physics.Raycast(ray, out hitInfo, distance, mask))
+        {
+            if (hitInfo.collider.GetComponent<Interactable>() != null)
+            {
+                Interactable interactable = hitInfo.collider.GetComponent<Interactable>();
+                playerUI.UpdateText(interactable.promtMessage);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Debug.Log("E key pressed");
+                    interactable.BaseInteract();
+                    Debug.Log("Interacting with " + interactable.name);
+                }
+            }
+        }
+    }
+}
