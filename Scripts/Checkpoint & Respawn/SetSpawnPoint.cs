cc0112e08c3aa3087898c755982cc34aa652b327
Vadim Niedental <vadim.niedental@gmail.com>
2024-10-02 13:49:44 +0200

    Currently Bugged

--- Scripts/Checkpoint & Respawn/SetSpawnPoint.cs
+++ Scripts/Checkpoint & Respawn/SetSpawnPoint.cs
@@ -1,18 +1,18 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class SetSpawnPoint : MonoBehaviour
-{
-    public GameObject newSpawnPoint; 
+// public class SetSpawnPoint : MonoBehaviour
+// {
+//     public GameObject newSpawnPoint; 
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            PlayerSpawn playerSpawn = other.GetComponent<PlayerSpawn>();
-            if (playerSpawn != null)
-            {
-                playerSpawn.SetSpawnPoint(newSpawnPoint.transform.position);
-            }
-        }
-    }
-}
+//     void OnTriggerEnter(Collider other)
+//     {
+//         if (other.CompareTag("Player"))
+//         {
+//             PlayerSpawn playerSpawn = other.GetComponent<PlayerSpawn>();
+//             if (playerSpawn != null)
+//             {
+//                 playerSpawn.SetSpawnPoint(newSpawnPoint.transform.position);
+//             }
+//         }
+//     }
+// }
